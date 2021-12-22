3c0a371b0bfa23e6c8c5eafd048065fad0f3ecdd
Mariusz Walania <mariusz.walania@gmail.com>
2021-12-22 10:56:34 +0100

    jupiter and mockito

---
+++ src/test/java/walaniam/scrabble/dictionary/DictionaryTest.java
@@ -0,0 +1,24 @@
+package walaniam.scrabble.dictionary;
+
+import org.junit.jupiter.api.Test;
+
+class DictionaryTest {
+
+
+
+    @Test
+    void findWords() {
+    }
+
+    @Test
+    void findStartingWith() {
+    }
+
+    @Test
+    void totalWords() {
+    }
+
+    @Test
+    void longestWordLength() {
+    }
+}
